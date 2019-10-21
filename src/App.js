624b9c200ee899427d2123e8b4f8c2eba4f7f460
Rad <chihaby@gmail.com>
2019-10-21 09:09:42 -0700

    completed

--- src/App.js
+++ src/App.js
@@ -3,21 +3,61 @@ import './App.css';
 import { database } from './firebase';
 
 class App extends Component {
-  state = {
-    data: null
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      newData: ''
+    };
+    this.dataRef = 'null';
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.dataRef = database.ref('/WOW/LOL/hehe');
+
+    database.ref().on('value', snapshot => {
+      this.setState({ data: snapshot.val() });
+    });
+  }
+
+  handleChange = event => {
+    const newData = event.target.value;
+    this.setState({
+      newData
+    });
   };
 
-  componentDidMount()  {
-    database.ref().on('value', (snapshot) => {
-      this.setState({data: snapshot.val()})
-    })
-  }
+  handleSubmit = event => {
+    event.preventDefault();
+    this.dataRef.push(this.state.newData);
+
+    // ------METHOD-1-----------------------
+    // database
+    //   .ref()
+    //   .child('AMAZING DATA')
+    //   .push(this.state.newData);
+    // // .child or .parent are used to navigate up and down the tree
+    //---------------------------------------
+
+    // ------METHOD-2------------------------
+    // database.ref('AMAZINGNEWDATA/hello/world/this/is/cool')
+    //         .push(this.state.newData);
+    //----------------------------------------
+  };
+
   render() {
     return (
-      <div>{JSON.stringify(this.state.data, null, 2)}</div>
-    )
+      <>
+        <div>{JSON.stringify(this.state.data, null, 2)}</div>
+        <form className="App-form" onSubmit={this.handleSubmit}>
+          <input type="text" onChange={this.handleChange} />
+          <input type="submit" />
+        </form>
+      </>
+    );
   }
-
 }
 
 export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,16 @@
 import firebase from 'firebase';
 
-
+  const firebaseConfig = {
+    apiKey: #####
+    authDomain: #####
+    databaseURL: #####
+    projectId: #####
+    storageBucket: #####
+    messagingSenderId: #####
+    appId: #####
+    measurementId: #####
+  };
+  firebase.initializeApp(firebaseConfig);
 
 export default firebase;
 
